0e39783d4594c0da55771ec7800f374c1808cb89
SorrawitS <152173667+SorrawitS@users.noreply.github.com>
2023-12-19 00:17:56 +0700

    101Update

---
+++ php101.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php  
+    $name = "GeeksforGeeks"; 
+    $number = 1;
+    $number_two = 2;
+    $keep_num = 0;
+    $keep_num = $number + $number_two;
+    echo "<h2>It Number : {$keep_num} </h2>";
+    echo "<h1>Hello User, </h1> <p>Welcome to {$name}</p>"; 
+?> 
+</body>
+</html>
