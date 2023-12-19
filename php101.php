176308f63f1435f0049ca3d2b8fbcad92ace4929
SorrawitS <152173667+SorrawitS@users.noreply.github.com>
2023-12-19 21:37:23 +0700

    101

--- php101.php
+++ php101.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="php101.css">
 </head>
+
 <body>
-<?php  
-    $name = "GeeksforGeeks"; 
-    $number = 1;
-    $number_two = 2;
-    $keep_num = 0;
-    $keep_num = $number + $number_two;
-    echo "<h2>It Number : {$keep_num} </h2>";
-    echo "<h1>Hello User, </h1> <p>Welcome to {$name}</p>"; 
-?> 
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <div class = "hey_love">
+                
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                 
+                echo " {$multi_x}x $x = $num <br>";
+                
+              }
+            ?>
+            </div>
+        </tbody>
+    </table>
+  
 </body>
+
 </html>
