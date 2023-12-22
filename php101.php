5ce70670a39fd5f733b22f4aabb54946b6479f50
SorrawitS <152173667+SorrawitS@users.noreply.github.com>
2023-12-22 19:22:13 +0700

    yeahh done!

--- php101.php
+++ php101.php
@@ -7,21 +7,93 @@
 </head>
 
 <body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <div class = "box">
+    <h1>ตารางสูตรคูณแม่ 2 , 4 , 6 , 8 , 10 , 12 , 14 </h1>
+    </div>
+    <div class= "flex-setting">
+    <div>
+    <img src="https://i0.wp.com/wordzz.com/wp-content/uploads/2018/12/Happy-New-Year-Images-Gif.gif?resize=656%2C320&ssl=1" alt="">
+    </div>
 
-    <table>
-        <tbody>
-            <div class = "hey_love">
+    </div>
+    
+    <div class = "grid-setting">
+    <div class = "hey_love">
+    <?php $multi_x = 2; ?>
             <?php $num = 1;
             for ($x = 1; $x <= 24; $x++) {
                  $num = $multi_x * $x;
                 echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
               }
             ?>
-            </div>
-        </tbody>
-    </table>
+    </div>
+
+    <div class = "hey_love_two">
+    <?php $multi_x = 4; ?>
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
+              }
+            ?>
+    </div>
+
+    <div class = "hey_love_three">
+    <?php $multi_x = 6; ?>
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
+              }
+            ?>
+    </div>
+
+    <div class = "hey_love_four">
+    <?php $multi_x = 8; ?>
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
+              }
+            ?>
+    </div>
+
+    <div class = "hey_love_five">
+    <?php $multi_x = 10; ?>
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
+              }
+            ?>
+    </div>
+
+    <div class = "hey_love_six">
+    <?php $multi_x = 12; ?>
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
+              }
+            ?>
+    </div>
+
+    <div class = "hey_love_seven">
+        <div>
+        <?php $multi_x = 14; ?>
+            <?php $num = 1;
+            for ($x = 1; $x <= 24; $x++) {
+                 $num = $multi_x * $x;
+                echo "{$multi_x}&nbsp&nbsp;&nbsp; x &nbsp&nbsp;&nbsp;$x = &nbsp&nbsp;&nbsp;$num <br>";
+              }
+            ?>
+    </div>
+        </div>
+    
+
+    </div>
+        
+   
   
 </body>
 
