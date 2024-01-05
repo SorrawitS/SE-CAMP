ed97fe91e6bb8833af99400facf6e19b3c5041c0
SorrawitS <152173667+SorrawitS@users.noreply.github.com>
2024-01-05 11:37:08 +0700

    0.4

---
+++ new-laravel-project/resources/views/myfolder/homeworkDynamic.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>แม่สูตรคูณแบบ Dynamic เปลี่ยนค่าตามที่ input</title>
+</head>
+<body>
+    <div class = "Container-Box">
+        <h1>Multiplication Table</h1>
+    </div>
+
+    
+</body>
+</html>
