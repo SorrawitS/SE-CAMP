01a5c46d760ffbfebdfcbcdba0e714928caf96ec
inthegarden <65160357@go.buu.ac.th>
2024-01-29 18:07:37 +0700

    29/01/24

---
+++ new-laravel-project/app/Http/Controllers/MyAuth.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyAuth extends Controller
+{
+    function login_view(){
+
+    }
+    function login_progress(Request $req){
+
+    }
+
+    function logout_process(){
+
+    }
+
+    function register_view(){
+
+    }
+
+    function register_process(){
+
+    }
+
+
+}
